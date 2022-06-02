5bb62a1b542f9291acff1628506d9bd6e41e8cf6
mohit vamja <mohitvamja1991@gmail.com>
2022-06-02 12:23:27 +0530

    task_02 change

--- CheckoutChamp/Meta/Model/GetProductData.php
+++ CheckoutChamp/Meta/Model/GetProductData.php
@@ -131,7 +131,7 @@ class GetProductData implements GetAllProduct
                 $product_data['items'][$i] = $value;
                 $i++;
                 $product_data['currency'] = $this->storeManager->getStore()->getBaseCurrencyCodeLatest();
-                $product_data['currency'] = $this->storeManager->getStore()->getCode1();                
+                $product_data['currency'] = $this->storeManager->getStore()->getCode();                
             }
         }else {
             $product_data = "The consumer isn't authorized to access %resources.";
